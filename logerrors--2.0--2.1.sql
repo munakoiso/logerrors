9ffe0d70943331c11ccadfce6f56b50bb8cb1926
Matěj Klonfar <klonfar.matej@ifortuna.cz>
2023-01-31 09:49:01 +0100

    Add sqlstate to the ouptput of pg_log_errors_stats()

---
+++ logerrors--2.0--2.1.sql
@@ -0,0 +1,15 @@
+ALTER EXTENSION logerrors DROP FUNCTION pg_log_errors_stats();
+DROP FUNCTION IF EXISTS pg_log_errors_stats();
+
+CREATE FUNCTION pg_log_errors_stats(
+    OUT time_interval integer,
+    OUT type text,
+    OUT message text,
+    OUT count integer,
+    OUT username text,
+    OUT database text,
+    OUT sqlstate text
+)
+    RETURNS SETOF record
+AS 'MODULE_PATHNAME', 'pg_log_errors_stats'
+    LANGUAGE C STRICT;
