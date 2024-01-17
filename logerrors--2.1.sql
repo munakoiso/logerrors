a603c89e8d01d6447c933be9179cc2ee97d2d352
Leonid Borchuk <xifos@yandex-team.ru>
2024-01-17 15:45:44 +0300

    Add pg_slow_log_stats

--- logerrors--2.1.sql
+++ logerrors--2.1.sql
@@ -17,3 +17,12 @@ CREATE FUNCTION pg_log_errors_reset()
     RETURNS void
 AS 'MODULE_PATHNAME', 'pg_log_errors_reset'
     LANGUAGE C STRICT;
+
+CREATE FUNCTION pg_slow_log_stats(
+    OUT slow_count integer,
+    OUT reset_time timestamp
+)
+    RETURNS SETOF record
+AS 'MODULE_PATHNAME', 'pg_slow_log_stats'
+    LANGUAGE C STRICT;
+GRANT ALL ON FUNCTION pg_slow_log_stats() TO public;
